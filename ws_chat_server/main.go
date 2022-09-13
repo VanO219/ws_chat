5ce383a062d8f71416c51291a8acc2babbc020ed
Ivan Sinyavsky <ivan.godev@yandex.ru>
2022-09-13 15:24:58 +0300

    добавил логгер в сервер

--- ws_chat_server/main.go
+++ ws_chat_server/main.go
@@ -1,43 +1,51 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gobwas/ws"
 	"github.com/gobwas/ws/wsutil"
 	"github.com/labstack/echo/v4"
 	"log"
 	"net/http"
+	"os"
+)
+
+var (
+	errorsLogger *log.Logger
 )
 
 func main() {
 	//e := echo.New()
 	//e.GET("/ws", WsUpdate)
 	//log.Fatalln(e.Start("192.168.1.3:4200"))
+	l, err := os.OpenFile("./errors.log", os.O_CREATE|os.O_APPEND|os.O_SYNC|os.O_TRUNC, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	errorsLogger = log.New(l, "error", 0)
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		conn, _, _, err := ws.UpgradeHTTP(r, w)
 		if err != nil {
-			fmt.Println("ничё не вышло с ws upgrade")
+			errorsLogger.Println("ничё не вышло с ws upgrade")
 		}
 		go func() {
 			defer conn.Close()
-
 			for {
 				msg, op, err := wsutil.ReadClientData(conn)
 				if err != nil {
-					fmt.Println(err)
+					errorsLogger.Println(err)
 					return
 				}
 				err = wsutil.WriteServerMessage(conn, op, msg)
 				if err != nil {
-					fmt.Println(err)
+					errorsLogger.Println(err)
 					return
 				}
 			}
 		}()
 	})
-	err := http.ListenAndServe("192.168.1.3:4200", nil)
+	err = http.ListenAndServe("192.168.1.3:4200", nil)
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+		errorsLogger.Fatal("ListenAndServe: ", err)
 	}
 }
 
