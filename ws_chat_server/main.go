ac9526f25197a0a8d83785ac780fa5fcb5485b08
Ivan Sinyavsky <ivan.godev@yandex.ru>
2022-11-16 12:14:06 +0300

    x

--- ws_chat_server/main.go
+++ ws_chat_server/main.go
@@ -6,14 +6,16 @@ import (
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 	"log"
 	"net/http"
 	"os"
 )
 
 var (
-	errorsLogger *log.Logger
-	upgrader     = websocket.Upgrader{
+	//lg *lg.lgger
+	lg       *logrus.Logger
+	upgrader = websocket.Upgrader{
 		CheckOrigin: func(r *http.Request) bool {
 			return true // Пропускаем любой запрос
 		},
@@ -23,40 +25,49 @@ var (
 func main() {
 	//e := echo.New()
 	//e.GET("/ws", WsUpdate)
-	//log.Fatalln(e.Start("192.168.1.3:4200"))
-	l, err := os.OpenFile("./errors.log", os.O_CREATE|os.O_APPEND|os.O_SYNC|os.O_TRUNC, 0666)
+	//lg.Fatalln(e.Start("192.168.1.3:4200"))
+	l, err := os.OpenFile("errors.log", os.O_CREATE|os.O_APPEND|os.O_SYNC|os.O_TRUNC|os.O_RDWR, 0777)
 	if err != nil {
 		log.Fatal(err)
 	}
-	errorsLogger = log.New(l, "error", 0)
+	lg = logrus.New()
+	lg.SetOutput(l)
+	lg.SetFormatter(&logrus.TextFormatter{})
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		connection, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
-			errorsLogger.Println("ничё не вышло с ws upgrade")
+			lg.Println("ничё не вышло с ws upgrade")
+			return
 		}
 		go func() {
 			defer connection.Close()
 			for {
 				mt, message, err := connection.ReadMessage()
-				if err != nil {
-					errorsLogger.Println(errors.Wrap(err, "connection.ReadMessage()"))
+				if err != nil || mt == websocket.CloseMessage {
+					if err != nil {
+						lg.Println(errors.Wrap(err, "connection.ReadMessage()"))
+					}
 					return
 				}
-				if mt == websocket.CloseMessage {
-					errorsLogger.Println("Получен код закрытия соединения")
-					return
-				}
+				//if  {
+				//	lg.Println("Получен код закрытия соединения")
+				//	return
+				//}
+				//if len(message) != 0 {
+				//	lg.Println("Получено новое сообщение")
+				//}
 				err = connection.WriteMessage(websocket.TextMessage, message)
 				if err != nil {
-					errorsLogger.Println(errors.Wrap(err, "connection.WriteMessage(websocket.TextMessage, message)"))
+					lg.Println(errors.Wrap(err, "connection.WriteMessage(websocket.TextMessage, message)"))
 					return
 				}
 			}
 		}()
 	})
+	lg.Println("Старт сервера")
 	err = http.ListenAndServe("192.168.1.3:4200", nil)
 	if err != nil {
-		errorsLogger.Fatal("ListenAndServe: ", err)
+		lg.Fatal("ListenAndServe: ", err)
 	}
 }
 
